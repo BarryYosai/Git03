878ac4ee17af6cb9bda0bd9b628e1a655d3b70b1
Barry <424043151@qq.com>
2022-06-16 12:19:30 +0800

    First Commit

---
+++ src/main/java/com/atguigu/Git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.Git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git222");
+        System.out.println("Hello Git2");
+        System.out.println("Hello Git333");
+        System.out.println("Hello Git444");
+        System.out.println("Hello Git666");
+    }
+}
